20b6405a40d1ead1c8041a12f73edd615338f909
Dimpal31 <109729171+Dimpal31@users.noreply.github.com>
2023-07-26 15:02:11 +0530

    Add files via upload

---
+++ module1/design/main.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+ @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: Text(widget.title),
+      ),
+        body: Row(
+          children: [
+            GridView.count(
+                crossAxisCount: 3,
+                crossAxisSpacing: 4.0,
+                mainAxisSpacing: 8.0,
+
+                children: List.generate(choices.length, (index) {
+                  return Center(
+                    child: SelectCard(choice: choices[index]),
+                  );
+                }
+                ),
+            ),
+          ],
+        ),
+    );
+ }
+}
+
+  class Choice {
+    const Choice({required this.title, required this.icon});
+    final String title;
+    final IconData icon;
+}
+
+const List<Choice> choices = const <Choice>[
+  const Choice(title: 'Home', icon: Icons.home),
+  const Choice(title: 'Contact', icon: Icons.contacts),
+  const Choice(title: 'Map', icon: Icons.map),
+  const Choice(title: 'Phone', icon: Icons.phone),
+  const Choice(title: 'Camera', icon: Icons.camera_alt),
+  const Choice(title: 'Setting', icon: Icons.settings),
+  const Choice(title: 'Album', icon: Icons.photo_album),
+  const Choice(title: 'WiFi', icon: Icons.wifi),
+];
+
+class SelectCard extends StatelessWidget {
+  const SelectCard({ Key? key, required this.choice}) : super(key: key);
+  final Choice choice;
+
+  @override
+  Widget build(BuildContext context) {
+    final TextStyle? textStyle = Theme.of(context).textTheme.displaySmall;
+    return Card(
+        color: Colors.orange,
+        child: Center(child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Expanded(child: Icon(choice.icon, size:50.0, color: textStyle?.color)),
+              Text(choice.title, style: textStyle),
+            ]
+        ),
+        )
+    );
+  }
+}
